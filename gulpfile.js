6d43890c231fdd4b96c5ce883b0f2527bb59da0f
MrrrTian <nevertback@163.com>
2018-03-08 11:06:57 +0800

    fix bug ios click

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ inlinesource = require('gulp-inline-source'),
 sourcemaps = require('gulp-sourcemaps'),
 babel = require("gulp-babel");
 //当前开发目录
-var nowPath = '/club/';// /others/queryArticle/
+var nowPath = '/gswap/';// /others/queryArticle/
 //var nowPath = '/gswap/';
 //根目录
 var rootpath = 'projects' + nowPath;
